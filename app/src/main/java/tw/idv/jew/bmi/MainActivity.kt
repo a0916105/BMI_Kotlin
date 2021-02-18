892a8988752466c7694f0ca4a6db06475ebb7e05
a0916105 <whdofjew@gmail.com>
2021-02-18 16:07:47 +0800

    setOnClickListener

--- app/src/main/java/tw/idv/jew/bmi/MainActivity.kt
+++ app/src/main/java/tw/idv/jew/bmi/MainActivity.kt
@@ -18,6 +18,14 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
 //        setContentView(R.layout.activity_main)
         setContentView(binding.root)
+
+        binding.bHelp.setOnClickListener {
+            AlertDialog.Builder(this)
+                    .setTitle("BMI說明")
+                    .setMessage("體重(kg)/身高的平方(m)")
+                    .setPositiveButton("OK", null)
+                    .show()
+        }
     }
 
     fun bmi(view: View){
@@ -25,12 +33,17 @@ class MainActivity : AppCompatActivity() {
         val height = binding.edHeight.text.toString().toFloat()
         val bmi = weight / (height * height)
         Log.d("BMI", bmi.toString())
+
         Toast.makeText(this, bmi.toString(), Toast.LENGTH_LONG).show()
+
+        //產生Builder物件
         AlertDialog.Builder(this)
+                //呼叫setMessage()方法設定顯示文字
                 .setMessage(bmi.toString())
-                .setTitle("Your BMI")
-                .setPositiveButton("OK", null)
-                .setNeutralButton("Cancel", null)
+                .setTitle(getString(R.string.your_bmi))
+                .setPositiveButton(getString(R.string.ok), null)
+                .setNeutralButton(getString(R.string.cancel), null)
+                //顯示對話框
                 .show()
     }
 }
